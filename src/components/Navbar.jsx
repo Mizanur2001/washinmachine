cfa5e5a24cd8405427b5a4e9016678efb720c527
Mizanur <jameslev2001@gmail.com>
2023-01-10 13:10:14 +0530

    fix the problem with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,47 @@ import React from 'react'
 import cartImg from '../assets/carts.png'
 import './css/Navbar.css'
 import logo from '../assets/logo.png'
-import menu from '../assets/menu.png'
-import close from '../assets/close.png'
 
 const Navbar = () => {
+    window.addEventListener('resize', function () {
+        addRequiredClass();
+    })
+
+    function addRequiredClass() {
+        const nav = document.querySelector('nav')
+        if (window.innerWidth < 800) {
+            nav.classList.add('mobile')
+        } else {
+            nav.classList.remove('mobile')
+        }
+    }
+
+    window.onload = addRequiredClass
+
+    let isActive = false;
+
+    const funcToggle = () => {
+        const element = document.querySelector(".listItem");
+        let bars = document.querySelectorAll('.hamburger span')
+        element.classList.toggle('open')
+        if (!isActive) {
+            bars[0].style.transform = 'rotate(45deg)'
+            bars[1].style.opacity = '0'
+            bars[2].style.transform = 'rotate(-45deg)'
+            isActive = true;
+        }
+        else {
+            bars[0].style.transform = 'rotate(0deg)'
+            bars[1].style.opacity = '1'
+            bars[2].style.transform = 'rotate(0deg)'
+            isActive = false;
+        }
+
+    }
+
     return (
         <>
-            <nav className='active'>
+            <nav className=''>
                 <div className='logo'>
                     <img src={logo} alt="Logo" />
                 </div>
@@ -24,9 +58,10 @@ const Navbar = () => {
                         <img src={cartImg} alt="cart" />
                     </div>
                 </div>
-                <div className='phoneNavMenu'>
-                    <img className='menuIcon' src={menu} alt="" />
-                    <img className='closeIcon' src={close} alt="" />
+                <div className='hamburger' onClick={funcToggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
             </nav>
         </>
