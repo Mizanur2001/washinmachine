8e2fb05c76d4df2aaacfb412e290b54d798b9cee
Mizanur <jameslev2001@gmail.com>
2023-01-22 21:38:49 +0530

    create about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './css/AboutUs.css'
+import aboutusHeroImg from '../assets/aboutusHeroSec.png'
 
 const AboutUs = () => {
     return (
         <div className='aboutUsContainer'>
-            
+            <div className='backGroundRapper'>
+                <div className='leftBg'></div>
+                <div className='rightBg'></div>
+            </div>
+            <div className='heroSection'>
+                <div className='textSection'>
+                    <h2>About Us</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores consectetur adipisicing elit. Asperiores</p>
+                </div>
+                <img src={aboutusHeroImg} alt="AbboutUs" />
+            </div>
         </div>
     )
 }
